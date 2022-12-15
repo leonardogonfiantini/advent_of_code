b4b823126a064af09b94556ee44e8d4a34af06ff
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-12-15 14:56:20 +0100

    part one day5 completed

--- 2022/day5/main.go
+++ 2022/day5/main.go
@@ -4,34 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
-
-type node struct {
-	value string
-	next *node
-}
-
-type stack struct {
-	start *node
-}
-
-func (s *stack) push_init(val string) {
-
-	new_node := &node {
-		val, 
-		nil,
-	}
-
-	if s.start == nil {
-		s.start = new_node
-	} else {
-		tmp := s.start
-		for tmp != nil {
-			tmp = tmp.next
-		}
-		tmp = new_node
-	}
-}
 
 func main() {
 
@@ -45,14 +20,48 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
-	scanner.Scan()
-
-	for i := 1; i < 33; i+=4 { 
-		fmt.Println(string(scanner.Text()[i]))
+	crates := make([][]string, 9)
+	for i:=0; i<9; i++ {
+		crates[i] = make([]string, 0)
 	}
 
+	t := 0
+	for scanner.Scan() {
+		j := 0
+		for i := 1; i <= 33; i+=4 { 
+			if string(scanner.Text()[i]) != " " {
+				crates[j] = append(crates[j], string(scanner.Text()[i]))
+			}
+			j++
+		}
 
+		t++
+		if t == 8 {
+			break
+		}
 
+	}
 
+	scanner.Scan()
+	scanner.Scan()
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+
+		move, _ := strconv.Atoi(line[1])
+		from, _ := strconv.Atoi(line[3])
+		to, _ := strconv.Atoi(line[5])
+
+		for move != 0 {
+			crate := crates[from-1][0]
+			crates[from-1] = crates[from-1][1:]
+			crates[to-1] = append([]string{crate}, crates[to-1]...)
+			move--
+		}
+	}
+
+	for i:=0; i<9; i++ {
+		fmt.Println(crates[i][0])
+	}
 
 }
