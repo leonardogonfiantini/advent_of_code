7370564e944253d9b9511dac687615e0fc5dff19
leonardogonfiantini <lp.gonfiantini@gmail.com>
2023-12-04 17:42:55 +0100

    day 2

--- 2023/day1/main.zig
+++ 2023/day1/main.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const print = @import("std").debug.print;
 
+const digits = [_][]const u64{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-const digits = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-
-fn solution(line: []const u8) u8 {
-    var digitOne: ?u8 = null;
-    var digitTwo: ?u8 = null;
+fn solution(line: []const u64) u64 {
+    var digitOne: ?u64 = null;
+    var digitTwo: ?u64 = null;
     for (line, 0..) |c, i| {
-        var digit: ?u8 = null;
+        var digit: ?u64 = null;
         if (c >= '0' and c <= '9') {
             digit = c - '0';
         } else {
             for (digits, 1..) |numStr, d| {
                 const lineSlice = line[i..@min(i + numStr.len, line.len)];
-                if (std.mem.eql(u8, numStr, lineSlice)) {
+                if (std.mem.eql(u64, numStr, lineSlice)) {
                     digit = @intCast(d);
                 }
             }
@@ -32,20 +31,18 @@ fn solution(line: []const u8) u8 {
 }
 
 pub fn main() !void {
-    
     var file = try std.fs.cwd().openFile("input.txt", .{});
     defer file.close();
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
-    var buf: [1024]u8 = undefined;
+    var buf: [1024]u64 = undefined;
     var sum: u64 = 0;
 
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         sum += solution(line);
-    }   
+    }
 
     print("{}", .{sum});
-
 }
--- 2023/day2/main.zig
+++ 2023/day2/main.zig
@@ -4,26 +4,56 @@ const print = std.debug.print;
 pub fn main() !void {
     var file = try std.fs.cwd().openFile("input.txt", .{});
     defer file.close();
-    
+
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
-    
+
     var buf: [1024]u8 = undefined;
-    const red_balls: u8 = 12;
-    _ = red_balls;
-    const green_balls: u8 = 13;
-    _ = green_balls;
-    const blue_balls: u8 = 14;
-    _ = blue_balls;
-    
+    const red_balls: u64 = 12;
+    const green_balls: u64 = 13;
+    const blue_balls: u64 = 14;
+
+    var sum: u64 = 0;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        print("{s}\n", .{line});
-    
+        var flag: u64 = 0;
         //only 12 red cubes, 13 green cubes, and 14 blue cubes
         var dotdot_split = std.mem.split(u8, line, ":");
-        while (dotdot_split.next()) |split| {
-            print("{s}\n", .{split});
+        const split_space = dotdot_split.next().?;
+        var game_name = std.mem.split(u8, split_space, " ");
+        _ = game_name.next();
+        const game_value = game_name.next().?;
+
+        while (dotdot_split.next()) |dotdot| {
+            var dotcomma_split = std.mem.split(u8, dotdot, ";");
+            while (dotcomma_split.next()) |dotcomma| {
+                var comma_split = std.mem.split(u8, dotcomma, ",");
+                while (comma_split.next()) |comma| {
+                    print("{s}\n", .{comma});
+                    var space_split = std.mem.split(u8, comma, " ");
+                    print("{s}\n", .{space_split.next().?});
+                    const ball_value: u64 = try std.fmt.parseInt(u64, space_split.next().?, 10);
+                    const color = space_split.next().?;
+
+                    if (std.mem.eql(u8, color, "red") and ball_value > red_balls) {
+                        flag = 1;
+                    }
+
+                    if (std.mem.eql(u8, color, "green") and ball_value > green_balls) {
+                        flag = 1;
+                    }
+
+                    if (std.mem.eql(u8, color, "blue") and ball_value > blue_balls) {
+                        flag = 1;
+                    }
+                }
+            }
+        }
+
+        if (flag == 0) {
+            const val = try std.fmt.parseInt(u64, game_value, 10);
+            sum += val;
         }
     }
 
+    print("{}", .{sum});
 }
